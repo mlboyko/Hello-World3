b32765fac2b69c0dfc0a9559ff1860588fbac688
makaylaboyko <makayla.boyko@maine.edu>
2018-02-12 09:21:36 -0500

    Added date display for user

--- Hello-World-3/src/HelloWorld.java
+++ Hello-World-3/src/HelloWorld.java
@@ -1,8 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+		Calendar date = Calendar.getInstance();
+		
+		System.out.println("Hello World! The current date is " + sdf.format(date.getTime()));
 		System.out.println("Hello, Alice!");
 		System.out.println("Hello, Bob!");
 		System.out.println("Hello, Charlie...");
